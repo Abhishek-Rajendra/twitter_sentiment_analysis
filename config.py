c28239f6c3a7f7294f7b63f0f37a4e0d893d11c1
Abhishek-Rajendra <rabhi1604@gmail.com>
2022-05-12 11:05:18 -0500

    removed password for elastic and bearer_token

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ elastic_url = "https://localhost:9200"
 host = "localhost"
 port = 9200
 user = "elastic"
-password = "y31tB4rL8+YdxZWOsSR0"
+# password = "elastic_search_password"
 index = "tweet_sentimental_analysis"
 indices = [index]
 
@@ -24,11 +24,5 @@ checkpoint_clustering = "checkpoint/clustering"
 hashtags = ["doctorstrangeinthemultiverseofmadness", "RRR", "kgf"]
 
 """API ACCESS KEYS"""
-
-access_token = "1429643104945065989-vczgyYnfcx3ZFX8g1BU07mStRefSEX"
-access_secret = "2cXOFIf6vYbF1TVvk1OGl3nFd5gsondxnEko2FlYHpKtX"
-consumer_key = "pd9GqEyuOlRzo2a1PopFkbrIi"
-consumer_secret = "aQ9dtDJmbC9VcDcxrwCeFQEw7U9AjUDdkxQFX7XxBj8jHVLFzo"
-
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAK1ybgEAAAAAJ18tuG7POzsOYYXvN85EgbR%2FTHA%3DnZVppVqUrg2KqJaoujDUa3q0WHJdvyAv72lTRfcv2u78D9JcsP"
+bearer_token = "generate_from_twitter_api_developers"
 
